1a8768949cf7c1913b43db9c0edd8c5033dd291d
veganmosquito <fruqtoza@gmail.com>
2023-06-12 18:07:44 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import validator from 'validator';
+console.log(validator.isEmail("test@test.com"))
+console.log(validator.isEmail("abcDE123"))
+console.log(validator.isEmail("Indian.helped@me.org"))
